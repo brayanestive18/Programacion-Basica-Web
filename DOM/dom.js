99cd5350dae52da640b56192bc8a61bd88dd18ee
Brayan Diaz <brayanestive18@gmail.com>
2019-07-31 16:57:21 -0500

    Creacion del boton y el input

--- DOM/dom.js
+++ DOM/dom.js
@@ -1,5 +1,11 @@
 var d = document.getElementById("lienzo");
 var lienzo = d.getContext("2d");
+var txtNumLines = document.getElementById("txt_number_lines");
+var btndraw = document.getElementById("button_draw");
+
+btndraw.addEventListener("click", drawClick);
+d.width = 600;
+d.height  = 600;
 
 function drawLine(color_line, xi, yi, xf, yf, lineWidth)
 {
@@ -15,24 +21,54 @@ function drawLine(color_line, xi, yi, xf, yf, lineWidth)
 function drawEgde()
 {
   lienzo.beginPath();
-  lienzo.strokeStyle = "black"
+  lienzo.strokeStyle = "#000000"
   lienzo.lineWidth = 8;
-  lienzo.rect(0, 0, 600, 600);
+  lienzo.rect(1, 1, 599, 599);
   lienzo.stroke();
   lienzo.closePath();
 }
 
-var widthLienzo = 600;
+function drawCross(step)
+{
+  var x = 300;
+  var x1 = 300;
+  for(var y = 60; y <= 300; y += step)
+  {
+    x += step;
+    x1 -= step;
+    drawLine("red", 300, y, x, 300, 2);
+    drawLine("blue", 300, y, x1, 300, 2);
+  }
+
+  x = 300;
+  x1 = 300;
+  for(var y = 540; y >= 300; y -= step)
+  {
+    x += step;
+    x1 -= step;
+    drawLine("green", 300, y, x, 300, 2);
+    drawLine("yellow", 300, y, x1, 300, 2);
+  }
+}
+
+function drawClick()
+{
+  alert("Hola");
+}
+
+var widthLienzo = d.width;
 var step = 5;
+var step1 = 5;
+var parting = widthLienzo / step1;
 var x = 0;
 
-for(var y = 0; y <= widthLienzo; y+=step)
+for(var y = parting; y <= widthLienzo; y+=parting)
 {
-  x += step;
+  x += parting;
   drawLine("red", 0, y, x, widthLienzo, 2);
   drawLine("yellow", x, 0, widthLienzo, y, 2);
 }
-
+/*
 x = 0
 for(var y = widthLienzo; y >= 0; y-=step)
 {
@@ -40,25 +76,7 @@ for(var y = widthLienzo; y >= 0; y-=step)
   drawLine("green", 0, y, x, 0, 2);
   x +=step;
 }
-
-x = 300;
-var x1 = 300;
-for(var y = 60; y <= 300; y += step)
-{
-  x += step;
-  x1 -= step;
-  drawLine("red", 300, y, x, 300, 2);
-  drawLine("blue", 300, y, x1, 300, 2);
-}
-
-x = 300;
-x1 = 300;
-for(var y = 540; y >= 300; y -= step)
-{
-  x += step;
-  x1 -= step;
-  drawLine("green", 300, y, x, 300, 2);
-  drawLine("yellow", 300, y, x1, 300, 2);
-}
+*/
+drawCross(step);
 
 drawEgde();
