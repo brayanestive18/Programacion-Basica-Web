a1f7dfd769f360647e2972859033e7526741318b
Brayan Diaz <brayanestive18@gmail.com>
2019-07-31 08:15:37 -0500

    Dibujo en canvas con JavaScript

--- DOM/dom.js
+++ DOM/dom.js
@@ -1,13 +1,23 @@
 var d = document.getElementById("lienzo");
 var lienzo = d.getContext("2d");
 
-function paintLine(color, xi, yi, xf, yf, lineWidth)
+function drawLine(color_line, xi, yi, xf, yf, lineWidth)
 {
   lienzo.lineWidth = lineWidth;
   lienzo.beginPath();
-  lienzo.strokeStyle = color;
+  lienzo.strokeStyle = color_line;
   lienzo.moveTo(xi, yi);
   lienzo.lineTo(xf, yf);
+  lienzo.stroke();
+  lienzo.closePath();
+}
+
+function drawEgde()
+{
+  lienzo.beginPath();
+  lienzo.strokeStyle = "black"
+  lienzo.lineWidth = 8;
+  lienzo.rect(0, 0, 600, 600);
   lienzo.stroke();
   lienzo.closePath();
 }
@@ -19,15 +29,15 @@ var x = 0;
 for(var y = 0; y <= widthLienzo; y+=step)
 {
   x += step;
-  paintLine("red", 0, y, x, widthLienzo, 2);
-  paintLine("yellow", x, 0, widthLienzo, y, 2);
+  drawLine("red", 0, y, x, widthLienzo, 2);
+  drawLine("yellow", x, 0, widthLienzo, y, 2);
 }
 
 x = 0
 for(var y = widthLienzo; y >= 0; y-=step)
 {
-  paintLine("blue", widthLienzo, x, y, widthLienzo, 2);
-  paintLine("green", 0, y, x, 0, 2);
+  drawLine("blue", widthLienzo, x, y, widthLienzo, 2);
+  drawLine("green", 0, y, x, 0, 2);
   x +=step;
 }
 
@@ -37,8 +47,8 @@ for(var y = 60; y <= 300; y += step)
 {
   x += step;
   x1 -= step;
-  paintLine("red", 300, y, x, 300, 2);
-  paintLine("blue", 300, y, x1, 300, 2);
+  drawLine("red", 300, y, x, 300, 2);
+  drawLine("blue", 300, y, x1, 300, 2);
 }
 
 x = 300;
@@ -47,6 +57,8 @@ for(var y = 540; y >= 300; y -= step)
 {
   x += step;
   x1 -= step;
-  paintLine("green", 300, y, x, 300, 2);
-  paintLine("yellow", 300, y, x1, 300, 2);
+  drawLine("green", 300, y, x, 300, 2);
+  drawLine("yellow", 300, y, x1, 300, 2);
 }
+
+drawEgde();
