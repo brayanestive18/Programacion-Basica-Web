d7aa4c418e74c811bbc716c7a33b5863ec3d7323
Brayan Diaz <brayanestive18@gmail.com>
2019-08-01 16:04:59 -0500

    Crear html para hacer dibujo con el teclado

--- DOM/dom.js
+++ DOM/dom.js
@@ -1,11 +1,19 @@
 var d = document.getElementById("lienzo");
 var lienzo = d.getContext("2d");
 var txtNumLines = document.getElementById("txt_number_lines");
-var btndraw = document.getElementById("button_draw");
+var btn_draw = document.getElementById("button_draw");
+var btn_clean = document.getElementById("button_clean");
+var option_up_r = document.getElementById("check-conner-up-rigth");
+var option_up_l = document.getElementById("check-conner-up-left");
+var option_down_r = document.getElementById("check-conner-down-rigth");
+var option_down_l = document.getElementById("check-conner-down-left");
+var option_cross = document.getElementById("check-cross");
+var size_lienzo = document.getElementById("txt_size");
 
-btndraw.addEventListener("click", drawClick);
-d.width = 600;
-d.height  = 600;
+btn_draw.addEventListener("click", drawClick);
+btn_clean.addEventListener("click", cleanClick);
+d.width = 300;
+d.height  = 300;
 
 function drawLine(color_line, xi, yi, xf, yf, lineWidth)
 {
@@ -23,39 +31,98 @@ function drawEgde()
   lienzo.beginPath();
   lienzo.strokeStyle = "#000000"
   lienzo.lineWidth = 8;
-  lienzo.rect(1, 1, 599, 599);
+  lienzo.rect(1, 1, d.width - 1 , d.height - 1);
   lienzo.stroke();
   lienzo.closePath();
 }
 
-function drawCross(step)
+function drawCross(step, width_Lienzo)
 {
-  var x = 300;
-  var x1 = 300;
-  for(var y = 60; y <= 300; y += step)
+  var size_cross = width_Lienzo / 2;
+  var x = size_cross;
+  var x1 = size_cross;
+  step = step / 2;
+  for(var y = 0; y <= size_cross; y += step)
   {
     x += step;
     x1 -= step;
-    drawLine("red", 300, y, x, 300, 2);
-    drawLine("blue", 300, y, x1, 300, 2);
+    drawLine("red", size_cross, y, x, size_cross, 2);
+    drawLine("blue", size_cross, y, x1, size_cross, 2);
   }
 
-  x = 300;
-  x1 = 300;
-  for(var y = 540; y >= 300; y -= step)
+  x = width_Lienzo / 2;
+  x1 = width_Lienzo / 2;
+  for(var y = width_Lienzo; y >= size_cross; y -= step)
   {
     x += step;
     x1 -= step;
-    drawLine("green", 300, y, x, 300, 2);
-    drawLine("yellow", 300, y, x1, 300, 2);
+    drawLine("green", size_cross, y, x, size_cross, 2);
+    drawLine("yellow", size_cross, y, x1, size_cross, 2);
   }
 }
 
 function drawClick()
 {
-  alert("Hola");
+  var widthLienzo = parseInt(size_lienzo.value);
+  d.width = widthLienzo;
+  d.height  = widthLienzo;
+  var num_lines = parseInt(txtNumLines.value);
+  var parting = widthLienzo / num_lines;
+  if (num_lines > 0)
+  {
+    if (option_up_r.checked)
+    {
+      var x = 0;
+      for(var y = parting; y <= widthLienzo; y += parting)
+      {
+        x += parting;
+        drawLine("yellow", x, 0, widthLienzo, y, 2);
+      }
+    }
+    if (option_up_l.checked)
+    {
+      var x = 0;
+      for(var y = widthLienzo; y >= 0; y -= parting)
+      {
+        drawLine("green", 0, y, x, 0, 2);
+        x +=parting;
+      }
+    }
+    if (option_down_r.checked)
+    {
+      var x = 0;
+      for(var y = widthLienzo; y >= 0; y -= parting)
+      {
+        drawLine("blue", widthLienzo, x, y, widthLienzo, 2);
+        x +=parting;
+      }
+    }
+    if (option_down_l.checked)
+    {
+      var x = 0;
+      for(var y = parting; y <= widthLienzo; y += parting)
+      {
+        x += parting;
+        drawLine("red", 0, y, x, widthLienzo, 2);
+      }
+    }
+    if (option_cross.checked)
+    {
+      drawCross(parting, widthLienzo);
+    }
+  }
+  drawEgde();
 }
 
+function cleanClick()
+{
+  lienzo.clearRect(0, 0, d.width, d.height);
+  d.width = 300;
+  d.height  = 300;
+  drawEgde();
+}
+
+/*
 var widthLienzo = d.width;
 var step = 5;
 var step1 = 5;
@@ -68,7 +135,7 @@ for(var y = parting; y <= widthLienzo; y+=parting)
   drawLine("red", 0, y, x, widthLienzo, 2);
   drawLine("yellow", x, 0, widthLienzo, y, 2);
 }
-/*
+
 x = 0
 for(var y = widthLienzo; y >= 0; y-=step)
 {
@@ -77,6 +144,6 @@ for(var y = widthLienzo; y >= 0; y-=step)
   x +=step;
 }
 */
-drawCross(step);
+
 
 drawEgde();
