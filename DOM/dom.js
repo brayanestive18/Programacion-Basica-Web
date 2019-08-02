6b3fc5cc706afc93e92f7e82bd77de2678bfe99a
Brayan Diaz <brayanestive18@gmail.com>
2019-08-01 23:12:31 -0500

    Creación de los campos para numero de lineas por figura y color

--- DOM/dom.js
+++ DOM/dom.js
@@ -1,6 +1,15 @@
 var d = document.getElementById("lienzo");
 var lienzo = d.getContext("2d");
-var txtNumLines = document.getElementById("txt_number_lines");
+var num_Lines_1 = document.getElementById("txt_number_lines_1");
+var num_Lines_2 = document.getElementById("txt_number_lines_2");
+var num_Lines_3 = document.getElementById("txt_number_lines_3");
+var num_Lines_4 = document.getElementById("txt_number_lines_4");
+var num_Lines_5 = document.getElementById("txt_number_lines_5");
+var color_line_1 = document.getElementById("txt_color_lines_1");
+var color_line_2 = document.getElementById("txt_color_lines_2");
+var color_line_3 = document.getElementById("txt_color_lines_3");
+var color_line_4 = document.getElementById("txt_color_lines_4");
+var color_line_5 = document.getElementById("txt_color_lines_5");
 var btn_draw = document.getElementById("button_draw");
 var btn_clean = document.getElementById("button_clean");
 var option_up_r = document.getElementById("check-conner-up-rigth");
@@ -66,47 +75,68 @@ function drawClick()
   var widthLienzo = parseInt(size_lienzo.value);
   d.width = widthLienzo;
   d.height  = widthLienzo;
-  var num_lines = parseInt(txtNumLines.value);
-  var parting = widthLienzo / num_lines;
-  if (num_lines > 0)
+  if (option_up_r.checked)
   {
-    if (option_up_r.checked)
+    var num_lines = parseInt(num_Lines_1.value);
+    var parting = widthLienzo / num_lines;
+    if(num_lines >= 0)
     {
       var x = 0;
       for(var y = parting; y <= widthLienzo; y += parting)
       {
         x += parting;
-        drawLine("yellow", x, 0, widthLienzo, y, 2);
+        drawLine(color_line_1.value, x, 0, widthLienzo, y, 2);
       }
     }
-    if (option_up_l.checked)
+
+  }
+  if (option_up_l.checked)
+  {
+    var num_lines = parseInt(num_Lines_2.value);
+    var parting = widthLienzo / num_lines;
+    if(num_lines >= 0)
     {
       var x = 0;
       for(var y = widthLienzo; y >= 0; y -= parting)
       {
-        drawLine("green", 0, y, x, 0, 2);
+        drawLine(color_line_2.value, 0, y, x, 0, 2);
         x +=parting;
       }
     }
-    if (option_down_r.checked)
+  }
+  if (option_down_r.checked)
+  {
+    var num_lines = parseInt(num_Lines_3.value);
+    var parting = widthLienzo / num_lines;
+    if(num_lines >= 0)
     {
       var x = 0;
       for(var y = widthLienzo; y >= 0; y -= parting)
       {
-        drawLine("blue", widthLienzo, x, y, widthLienzo, 2);
+        drawLine(color_line_3.value, widthLienzo, x, y, widthLienzo, 2);
         x +=parting;
       }
     }
-    if (option_down_l.checked)
+  }
+  if (option_down_l.checked)
+  {
+    var num_lines = parseInt(num_Lines_4.value);
+    var parting = widthLienzo / num_lines;
+    if(num_lines >= 0)
     {
       var x = 0;
       for(var y = parting; y <= widthLienzo; y += parting)
       {
         x += parting;
-        drawLine("red", 0, y, x, widthLienzo, 2);
+        drawLine(color_line_4.value, 0, y, x, widthLienzo, 2);
       }
     }
-    if (option_cross.checked)
+  }
+  if (option_cross.checked)
+  {
+    var num_lines = parseInt(num_Lines_5.value);
+    var parting = widthLienzo / num_lines;
+    if(num_lines >= 0)
     {
       drawCross(parting, widthLienzo);
     }
