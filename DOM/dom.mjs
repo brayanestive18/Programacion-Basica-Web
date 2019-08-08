9471c25a19a41cc5e77a5b1ecba4e8d3977a0f93
Brayan Diaz <brayanestive18@gmail.com>
2019-08-08 17:05:02 -0500

    Preguntar tamaño del canvas en draw free

--- DOM/dom.mjs
+++ DOM/dom.mjs
@@ -28,61 +28,6 @@ btn_clean.addEventListener("click", cleanClick);
 d.width = 300;
 d.height  = 300;
 
-// function drawLine(color_line, xi, yi, xf, yf, lineWidth)
-// {
-//   lienzo.lineWidth = lineWidth;
-//   lienzo.beginPath();
-//   lienzo.strokeStyle = color_line;
-//   lienzo.moveTo(xi, yi);
-//   lienzo.lineTo(xf, yf);
-//   lienzo.stroke();
-//   lienzo.closePath();
-// }
-
-// function drawEgde()
-// {
-//   lienzo.beginPath();
-//   lienzo.strokeStyle = "#000000"
-//   lienzo.lineWidth = 8;
-//   lienzo.rect(1, 1, d.width - 1 , d.height - 1);
-//   lienzo.stroke();
-//   lienzo.closePath();
-// }
-
-// function drawCross(step, width_Lienzo)
-// {
-//   var size_cross = width_Lienzo / 2;
-//   var x = size_cross;
-//   var x1 = size_cross;
-//   var aux = step;
-//   step = step / 2;
-//   aux *= 4;
-//   aux = parseInt(16777215 / aux);
-//   var color_cross = "#000000";
-//   var i = 0;
-//   for(var y = 0; y <= size_cross; y += step)
-//   {
-//     x += step;
-//     x1 -= step;
-//     drawLine(color_cross, size_cross, y, x, size_cross, 2);
-//     aux += aux;
-//     color_cross = "#" + (aux).toString(16);
-//     drawLine(color_cross, size_cross, y, x1, size_cross, 2);
-//     aux += aux;
-//     color_cross = "#" + (aux).toString(16);
-//     i += 2;
-//   }
-//   x = width_Lienzo / 2;
-//   x1 = width_Lienzo / 2;
-//   for(var y = width_Lienzo; y >= size_cross; y -= step)
-//   {
-//     x += step;
-//     x1 -= step;
-//     drawLine("green", size_cross, y, x, size_cross, 2);
-//     drawLine("yellow", size_cross, y, x1, size_cross, 2);
-//   }
-// }
-
 function drawClick()
 {
   var widthLienzo = parseInt(size_lienzo.value);
